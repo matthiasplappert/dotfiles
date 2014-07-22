6807e1246f22ce7ef48304777e74b8117ae15f2d
Matthias Plappert <matthiasplappert@me.com>
2014-07-22 16:41:47 +0200

    Update installer.

--- install.sh
+++ install.sh
@@ -10,6 +10,10 @@ ln -s ~/.dotfiles/vimrc ~/.vimrc
 ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/zshrc ~/.zshrc
 
+# Checkout submodule
+git submodule sync; git submodule update --init --recursive;
+
 # setup vundle
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
+
