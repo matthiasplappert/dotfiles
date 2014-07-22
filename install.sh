c2a3c32b7e304b6e6e58d0c32dcffee182c1d82d
Matthias Plappert <matthiasplappert@me.com>
2014-07-22 16:36:56 +0200

    Update install script.

--- install.sh
+++ install.sh
@@ -1,14 +1,15 @@
 #! /bin/bash
 
-DOTFILES=$( cd $(dirname $0) ; pwd -P )
-
-# vim
-ln -sf $DOTFILES/vimrc ~/.vimrc
-ln -sf $DOTFILES/vim/colors ~/.vim/colors
-mkdir -p $HOME/.vim/undo
-mkdir -p $HOME/.vim/backups
-mkdir -p $HOME/.vim/swaps
+# setup dotfile structure
+mkdir ~/.vim
+mkdir ~/.vim/backups
+mkdir ~/.vim/undo
+mkdir ~/.vim/swaps
+ln -s ~/.dotfiles/vim/colors ~/.vim/colors
+ln -s ~/.dotfiles/vimrc ~/.vimrc
+ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
+ln -s ~/.dotfiles/zshrc ~/.zshrc
 
 # setup vundle
-git clone https://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
