6f150c7bec7b4f66014d858409e162d18d56c2d3
Matthias Plappert <matthiasplappert@me.com>
2016-02-17 11:11:52 +0100

    Only enable H2T stuff on lab computers

--- aliases/h2t.sh
+++ aliases/h2t.sh
@@ -1,2 +1,4 @@
-source $HOME/Code/armarx/MiraculiX/_setupAliases.sh
-alias subl="~/Applications/sublime_text_3/sublime_text"
+if [ -d "$HOME/Code/armarx" ]; then
+	source $HOME/Code/armarx/MiraculiX/_setupAliases.sh
+	alias subl="$HOME/Applications/sublime_text_3/sublime_text"
+fi
