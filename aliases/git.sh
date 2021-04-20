3a483af9d89e1ee9636f194e21abb1721527de68
Matthias Plappert <matthiasplappert@me.com>
2021-04-19 17:01:32 -0700

    Clean up

--- aliases/git.sh
+++ aliases/git.sh
@@ -3,7 +3,6 @@ alias gs="git status"
 alias gdm='git difftool'
 alias gds='git difftool --staged'
 alias gsu='git submodule sync; git submodule update --init --recursive;'
-alias grm="git rm `git ls-files --deleted`"
 alias gbcu="git branch --merged | grep \"matthias/*\" | xargs -n 1 git branch -d"
 alias gcu='git clean -f -d'
 
--- aliases/h2t.sh
+++
@@ -1,4 +0,0 @@
-if [ -d "$HOME/Code/armarx" ]; then
-	source $HOME/Code/armarx/MiraculiX/_setupAliases.sh
-	alias subl="$HOME/Applications/sublime_text_3/sublime_text"
-fi
